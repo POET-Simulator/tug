96a2d1cc5bb5c7e0c31770ab67c9a5b29f510193
Max Luebke <mluebke@gfz-potsdam.de>
2021-12-01 17:59:08 +0100

    remove old library files

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "diffusion.hpp"
+#include "BTCSDiffusion.hpp"
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -11,15 +11,21 @@ int main(int argc, char *argv[]) {
 
   std::vector<double> alpha(x, 1 * pow(10, -1));
   std::vector<double> input(x, 1 * std::pow(10, -6));
-  std::vector<double> bc;
+  std::vector<double> bc_left, bc_right;
 
-  bc.push_back(5. * std::pow(10, -6));
-  bc.push_back(1. * std::pow(10, -6));
+  bc_left.push_back(5. * std::pow(10, -6));
+  bc_right.push_back(-1);
   // input[x + 2] = 5.5556554 * std::pow(10, -6);
   // input[x + 3] = 5.234564213 * std::pow(10, -6);
 
+  BTCSDiffusion diffu(x);
+
+  diffu.setBoundaryCondition(bc_left, BTCSDiffusion::LEFT);
+  diffu.setBoundaryCondition(bc_right, BTCSDiffusion::RIGHT);
+
   for (int i = 0; i < 100; i++) {
-    BTCS1D(x, input, alpha, 1., bc);
+    diffu.simulate(input, alpha, 1.);
+    // BTCS1D(x, input, alpha, 1., bc);
   }
 
   return 0;
