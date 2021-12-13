816a1cc67ab8b8bf88332a70d4f2d7f011547ad1
Max Lübke <mluebke@gfz-potsdam.de>
2021-12-13 19:39:22 +0100

    Implemented 1D diffusion with new data structure

--- src/BTCSDiffusion.hpp
+++ src/BTCSDiffusion.hpp
@@ -2,18 +2,30 @@
 #define BTCSDIFFUSION_H_
 
 #include <Eigen/Sparse>
+#include <tuple>
 #include <vector>
-
-/*!
- * Type defining the side of given boundary condition.
- */
-typedef int BCSide;
 
 /*!
  * Datatype to fill the sparse matrix which is used to solve the equation
  * system.
  */
 typedef Eigen::Triplet<double> T;
+
+/*!
+ * Defines both types of boundary condition as a datatype.
+ */
+typedef int bctype;
+
+/*!
+ * A boundary condition consists of two features. A type and the according
+ * value. Here we can differentiate between:
+ *
+ * - Neumann boundary conditon: type BC_NEUMANN with the value defining the
+ * gradient
+ * - Dirichlet boundary condition: type BC_DIRICHLET with the actual value of
+ * the boundary condition
+ */
+typedef std::vector<std::tuple<bctype, double>> boundary_condition;
 
 /*!
  * Class implementing a solution for a 1/2/3D diffusion equation using backward
@@ -23,21 +35,20 @@ class BTCSDiffusion {
 
 public:
   /*!
-   * Set left boundary condition.
+   * Defines a Neumann boundary condition.
    */
-  static const BCSide LEFT;
-
+  static const int BC_NEUMANN;
   /*!
-   * Set right boundary condition.
+   * Defines a Dirichlet boundary condition.
    */
-  static const BCSide RIGHT;
+  static const int BC_DIRICHLET;
 
   /*!
    * Create 1D-diffusion module.
    *
    * @param x Count of cells in x direction.
    */
-  BTCSDiffusion(int x);
+  explicit BTCSDiffusion(int x);
 
   /*!
    * Currently not implemented: Create 2D-diffusion module.
@@ -45,7 +56,7 @@ public:
    * @param x Count of cells in x direction.
    * @param y Count of cells in y direction.
    */
-  BTCSDiffusion(int x, int y);
+  explicit BTCSDiffusion(int x, int y);
 
   /*!
    * Currently not implemented: Create 3D-diffusion module.
@@ -54,18 +65,7 @@ public:
    * @param y Count of cells in y direction.
    * @param z Count of cells in z direction.
    */
-  BTCSDiffusion(int x, int y, int z);
-
-  /*!
-   * Sets internal boundary condition at the end of the grid/ghost zones.
-   * Currently only implemented for 1D diffusion.
-   *
-   * @param input Vector containing all the values to initialize the ghost
-   * zones.
-   * @param side Sets the side of the boundary condition. See BCSide for more
-   * information.
-   */
-  void setBoundaryCondition(std::vector<double> input, BCSide side);
+  explicit BTCSDiffusion(int x, int y, int z);
 
   /*!
    * With given ghost zones simulate diffusion. Only 1D allowed at this moment.
@@ -73,17 +73,51 @@ public:
    * @param c Vector describing the concentration of one solution of the grid as
    * continious memory (Row-wise).
    * @param alpha Vector of diffusioncoefficients for each grid element.
-   * @param timestep Time (in seconds ?) to simulate.
    */
-  void simulate(std::vector<double> &c, std::vector<double> &alpha,
-                double timestep);
+  void simulate(std::vector<double> &c, const std::vector<double> &alpha);
+
+  /*!
+   * Set the timestep of the simulation
+   *
+   * @param time_step Time step (in seconds ???)
+   */
+  void setTimestep(double time_step);
+
+  /*!
+   * Set the boundary condition of the given grid. This is done by defining an
+   * index (exact order still to be determined), the type of the boundary
+   * condition and the according value.
+   *
+   * @param index Index of the boundary condition vector.
+   * @param val Value of the boundary condition (gradient for Neumann, exact
+   * value for Dirichlet).
+   * @param Type of the grid cell.
+   */
+  void setBoundaryCondition(int index, double val, bctype type);
 
 private:
-  std::vector<double> bc;
+  void simulate1D(std::vector<double> &c, double bc_left, double bc_right,
+                  const std::vector<double> &alpha, double dx, int size);
+  void simulate2D(std::vector<double> &c);
+  void simulate3D(std::vector<double> &c);
+
+  double getBCFromTuple(int index, double nearest_value, double neighbor_alpha);
+
+  boundary_condition bc;
+
+  Eigen::SparseMatrix<double> A_matrix;
+  Eigen::VectorXd b_vector;
+  Eigen::VectorXd x_vector;
+
+  double time_step;
+
   int grid_dim;
-  int dim_x;
-  int dim_y;
-  int dim_z;
+  int n_x;
+  double dx;
+  int n_y;
+  double dy;
+  int n_z;
+  double dz;
 };
 
 #endif // BTCSDIFFUSION_H_
