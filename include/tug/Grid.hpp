16640fe122dcfcc4abd681a8b05bfef0c2d38fcb
philippun <philippungrund@icloud.com>
2023-07-13 10:54:48 +0200

    change: Grid.hpp | finished outline Grid class

--- include/tug/Grid.hpp
+++ include/tug/Grid.hpp
@@ -1,35 +1,62 @@
 #include <iostream>
-#include <vector>
+#include <Eigen/Core>
 
-using namespace std;
+using namespace Eigen;
 
 class Grid {
     public:
-    /**
-     * @brief Construct a new Grid object
-     * 
-     * @param n 
-     */
-    Grid(int n);
 
-    /**
-     * @brief Construct a new Grid object
-     * 
-     * @param n 
-     * @param m 
-     */
-    Grid(int n, int m);
+        /**
+        * @brief Construct a new Grid object
+        * 
+        * @param n 
+        */
+        Grid(int n);
 
-    /**
-     * @brief Set the Alpha object
-     * 
-     * @param alpha 
-     */
-    void setAlpha(vector<float> alpha);
+        /**
+        * @brief Construct a new Grid object
+        * 
+        * @param n 
+        * @param m 
+        */
+        Grid(int n, int m);
 
-    void setAlpha(vector<float> alpha_x, vector<float> alpha_y);
+        /**
+        * @brief Set the Concentrations object
+        * 
+        * @param concentrations 
+        */
+        void setConcentrations(Matrix2d concentrations);
+
+        /**
+         * @brief Get the Concentrations object
+         * 
+         * @return auto 
+         */
+        auto getConcentrations();
+
+        /**
+        * @brief Set the Alpha object
+        * 
+        * @param alpha 
+        */
+        void setAlpha(Matrix2d alpha);
+
+        /**
+        * @brief Set the Alpha object
+        * 
+        * @param alpha_x 
+        * @param alpha_y 
+        */
+        void setAlpha(Matrix2d alpha_x, Matrix2d alpha_y);
+        
 
     private:
-    
 
+        int dim;
+        int n;
+        int m;
+        Matrix2d concentrations;
+        Matrix2d alpha_x;
+        Matrix2d alpha_y;
 };
