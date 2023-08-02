d22f8cf71bce87677124dca7c1624a87ecbbee72
Hannes Signer <hm.signer@gmail.com>
2023-08-02 14:23:00 +0200

    pipeline test path

--- test/testSimulation.cpp
+++ test/testSimulation.cpp
@@ -61,7 +61,7 @@ TEST_CASE("equality to reference matrix") {
     fs::path p = fs::current_path().parent_path().parent_path();
     string test_path = p.generic_string() + "/test/FTCS_11_11_7000.csv";
     MatrixXd reference = CSV2Eigen(test_path);
-
+    cout << test_path << endl;
     Grid grid = setupSimulation();
     CHECK(checkSimilarity(reference, grid.getConcentrations(), 0.1) == true);
 }
