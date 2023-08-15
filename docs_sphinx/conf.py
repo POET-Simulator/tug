da7823f7fce7b92444a5a5ae893c94749f71b0d7
Hannes Signer <hm.signer@gmail.com>
2023-08-15 15:47:33 +0200

    update sphinx

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -40,7 +40,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx_sitemap',
     'sphinx.ext.inheritance_diagram',
     'sphinx_sitemap',
     'breathe'
