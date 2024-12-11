1a11991af0b52a58011569f3021753c3fa075696
Max Lübke <mluebke@uni-potsdam.de>
2024-12-11 15:30:10 +0100

    feat: Add unit tests for Velocities functionality

--- include/tug/tug.hpp
+++ include/tug/tug.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <tug/Advection/Advection.hpp>
+// #include <tug/Advection/Advection.hpp>
 #include <tug/Boundary.hpp>
 #include <tug/Core/Matrix.hpp>
 #include <tug/Diffusion/Diffusion.hpp>
